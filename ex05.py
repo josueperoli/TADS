af3d759d35a50ec2f23837ddd8cef26b20b7c511
Josué Pereira <josue.jpo2003@gmail.com>
2026-03-05 19:48:04 -0300

    exercicio for

---
+++ ex05.py
@@ -0,0 +1,11 @@
+amigos = ["Huguinho", "Zezinho", "Luizinho", "Zezinho"]
+
+for amigo in amigos:
+    print(amigo)
+
+
+amigos = ["Huguinho", "Zezinho", "Luizinho", "Zezinho"]
+
+for amigo in amigos:
+    print(f"Sou amigo do {amigo}")
+    print(f"Olá {amigo}, é muito bom ser seu amigo!")
