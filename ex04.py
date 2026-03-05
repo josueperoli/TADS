cf48b31ad0bde8a44501ccd87fcee0791475e16e
Josué Pereira <josue.jpo2003@gmail.com>
2026-03-05 19:36:38 -0300

    ordenação

--- ex04.py
+++ ex04.py
@@ -2,4 +2,13 @@ amigos = ["Huguinho", "Zezinho", "Luizinho", "Zezinho"]
 
 #Ordenação - Case sensitive (letras minusculas < maiusculas)
 amigos.sort()
+#print(amigos)
+
+#Ordenação case insensitive (não importa se é maiuscula ou minuscula, vai colocar em ordem)
+amigos.sort(key = str.lower)
+#print(amigos)
+
+amigos = ["Huguinho", "Zezinho", "Luizinho", "Zezinho"]
+print(sorted(amigos, reverse=True, key=str.upper))#cria uma lista com invertida, não sensitive
 print(amigos)
+
