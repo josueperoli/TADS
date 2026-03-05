4741feaffcffa548f0389948bb26aa8f201e8657
Josué Pereira <josue.jpo2003@gmail.com>
2026-03-05 20:12:37 -0300

    range

---
+++ ex06.py
@@ -0,0 +1,18 @@
+for value in range(1, 5):
+    print(value)
+print("\n")
+
+for value in range(5):
+    print(value)
+print("\n")
+
+for value in range(0, 10, 2):
+    print(value)
+print("\n")
+
+for value in range(10, 0, -1):
+    print(value)
+print("\n")
+
+numeros = list(range(0,10))
+print(numeros)
